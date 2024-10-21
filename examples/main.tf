cfacbef81265bf7a00f745a89f55b0cbd5c1caf1
Justin Zeyeum <justinzeyeum@gmail.com>
2024-10-21 12:28:20 -0400

    Added staging Module

--- examples/main.tf
+++ examples/main.tf
@@ -1,5 +1,5 @@
 module "test" {
-  source   = "github.com/fojiglobal/justin-tf-modules//test?ref=v1.0.1"
+  source   = "github.com/fojiglobal/justin-tf-modules//test?ref=v1.0.2"
   vpc_cidr = "10.25.0.0/16"
   env      = "test"
 }
